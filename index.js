0f0b0cc313a2944b0284971097ac680a631bf0ab
Elad Nava <eladnava@gmail.com>
2016-07-13 14:21:24 +0300

    Extract data object automatically

--- index.js
+++ index.js
@@ -158,6 +158,9 @@ CachetAPI.prototype.getComponentById = function (id) {
 
         // Execute request
         request(req, function (err, res, body) {
+            // Extract data object from body if it exists
+            body = (body.data) ? body.data : body;
+
             // Handle the response accordingly
             handleResponse(err, res, body, reject, resolve);
         });
